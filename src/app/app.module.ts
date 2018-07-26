82de847880934cb5f1602e16b9f56d274f0854bc
aconbol <aconbol@gmail.com>
2018-07-25 22:36:17 -0400

    linea base o2arquitectura sostenible

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { NavegacionComponent } from './shared/navegacion/navegacion.component';
+import { AboutComponent } from './shared/about/about.component';
+import { ServicesComponent } from './shared/services/services.component';
+import { TeamComponent } from './shared/team/team.component';
+import { PortfolioComponent } from './shared/portfolio/portfolio.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavegacionComponent,
+    AboutComponent,
+    ServicesComponent,
+    TeamComponent,
+    PortfolioComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/shared/navegacion/navegacion.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navegacion',
+  templateUrl: './navegacion.component.html'
+})
+export class NavegacionComponent {
+
+  constructor() { }
+
+}
