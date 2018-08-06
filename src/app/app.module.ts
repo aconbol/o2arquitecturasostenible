deac98f209e31d90d08543ac419d7d4ac8f0645e
aconbol <aconbol@gmail.com>
2018-08-05 20:12:17 -0400

    Linea base inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { NavegacionComponent } from './shared/navegacion/navegacion.component';
@@ -10,6 +12,8 @@ import { TeamComponent } from './shared/team/team.component';
 import { PortfolioComponent } from './shared/portfolio/portfolio.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { ClientsComponent } from './shared/clients/clients.component';
+import { PrincipalComponent } from './pages/principal/principal.component';
+import { ProyectosComponent } from './pages/proyectos/proyectos.component';
 
 @NgModule({
   declarations: [
@@ -21,10 +25,13 @@ import { ClientsComponent } from './shared/clients/clients.component';
     TeamComponent,
     PortfolioComponent,
     FooterComponent,
-    ClientsComponent
+    ClientsComponent,
+    PrincipalComponent,
+    ProyectosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
